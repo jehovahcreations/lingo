059b336381c9b26f5186f8c36aaa370caeb45195
Apple <apple@Apples-MacBook-Pro.local>
2024-06-15 11:26:14 +0530

    hhh

--- main.go
+++ main.go
@@ -305,7 +305,7 @@ func insertOne(client *mongo.Client, ctx context.Context, dataBase, col string, 
 func query(client *mongo.Client, ctx context.Context, dataBase, col string, query, field interface{})(result *mongo.Cursor, err error) {
     collection := client.Database(dataBase).Collection(col)
     result, err = collection.Find(ctx, query, 
-                                  options.Find().SetProjection(field).SetLimit(2))
+                                  options.Find().SetProjection(field))
     return
 }
 func UpdateOne(client *mongo.Client, ctx context.Context, dataBase, col string, filter, update interface{})(result *mongo.UpdateResult, err error) {
@@ -350,7 +350,7 @@ func GetHomePage(){
     }
 	fmt.Println(autos)
 	
-	for i := 0; i < 2; i++{ 
+	for i := 0; i < len(autos); i++{ 
        sendOtp(autos[i].Mobile,i)
     } 
 		fmt.Scanln()
